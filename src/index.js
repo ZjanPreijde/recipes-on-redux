b22167d16e126a447b7ca660ae6d388459e7ba01
ZjanPreijde <j.preijde2@upcmail.nl>
2018-02-07 15:37:15 +0100

    Initial setup Redux store (which is not a shop :-) )

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux'
+
 import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+import store from './store'
+
 import './index.css';
 
+
+// ReactDOM.render(
+//   <App />, document.getElementById('root')
+// );
 ReactDOM.render(
-  <App />, document.getElementById('root')
-);
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker();
